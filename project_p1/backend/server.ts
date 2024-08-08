aabbe6e84a7233e205fda8d25e52cc960b912770
bilal12367 <sk.bilal.md@gmail.com>
2024-08-08 01:50:28 +0000

    update

--- project_p1/backend/server.ts
+++ project_p1/backend/server.ts
@@ -1,9 +1,13 @@
 
 import express, { Express } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.configDotenv()
 
 const app: Express = express();
 
 
-app.listen(5000, () => {
-    console.log("Server Started listening at port 5000...")
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server Started listening at port ",process.env.PORT || 5000,"...")
 })
