bb1e2177230b0abeb9432c82aae2dcef73426743
bilal12367 <sk.bilal.md@gmail.com>
2025-07-26 15:16:53 +0530

    remove_node_modules

--- kafka_demo/api_gateway_kong/kong_start_docker.sh
+++ kafka_demo/api_gateway_kong/kong_start_docker.sh
@@ -1,42 +1,33 @@
-docker network create kong-net
+docker rm -f kong kong-db
 
-docker run -d --name kong-database \
- --network=kong-net \
- -p 5432:5432 \
- -e "POSTGRES_USER=kong" \
- -e "POSTGRES_DB=kong" \
- -e "POSTGRES_PASSWORD=kong" \
- postgres:13
+docker run -d --name kong-db \
+  --network host \
+  -e POSTGRES_USER=kong \
+  -e POSTGRES_DB=kong \
+  -e POSTGRES_PASSWORD=kong \
+  postgres:15
 
-docker run --rm --network=kong-net --name=kong-db \
--e "KONG_DATABASE=postgres" \
--e "KONG_PG_HOST=kong-database" \
--e "KONG_PG_PASSWORD=kong" \
--e "KONG_PASSWORD=kong" \
-kong/kong-gateway:3.10.0.1 kong migrations bootstrap
+docker run --rm \
+  --network host \
+  -e KONG_DATABASE=postgres \
+  -e KONG_PG_HOST=127.0.0.1 \
+  -e KONG_PG_USER=kong \
+  -e KONG_PG_PASSWORD=kong \
+  -e KONG_PG_DATABASE=kong \
+  kong:3.6 kong migrations bootstrap
 
 
-docker run -d --name kong-gateway \
---network=kong-net \
--e "KONG_DATABASE=postgres" \
--e "KONG_PG_HOST=kong-database" \
--e "KONG_PG_USER=kong" \
--e "KONG_PG_PASSWORD=kong" \
--e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
--e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
--e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
--e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
--e "KONG_ADMIN_LISTEN=0.0.0.0:8001" \
--e "KONG_ADMIN_GUI_URL=http://localhost:8002" \
--p 8000:8000 \
--p 8443:8443 \
--p 8001:8001 \
--p 8444:8444 \
--p 8002:8002 \
--p 8445:8445 \
--p 8003:8003 \
--p 8004:8004 \
-kong/kong-gateway:3.10.0.1
-
-docker run -p 3000:3000 --network kong-net -e JWT_KEY="BIISXG18ZlHzwvPLcW15dOYs9bZsQOW6" -e JWT_SECRET="wvFA1vI0gsfCTi5ZM5GioBltAoEbofMT" --name node_test_1 node_test_app:latest
-
+docker run -d --name kong \
+  --network host \
+  -e KONG_DATABASE=postgres \
+  -e KONG_PG_HOST=127.0.0.1 \
+  -e KONG_PG_USER=kong \
+  -e KONG_PG_PASSWORD=kong \
+  -e KONG_PG_DATABASE=kong \
+  -e KONG_PROXY_ACCESS_LOG=/dev/stdout \
+  -e KONG_ADMIN_ACCESS_LOG=/dev/stdout \
+  -e KONG_PROXY_ERROR_LOG=/dev/stderr \
+  -e KONG_ADMIN_ERROR_LOG=/dev/stderr \
+  -e KONG_PROXY_LISTEN=0.0.0.0:8000 \
+  -e KONG_ADMIN_LISTEN=0.0.0.0:8001 \
+  kong:3.6
